fb3708c71f8e53505831d690ba7e4dcf838a01f2
bill_ju <zhangjuyuan2020@163.com>
2024-04-11 14:59:05 +0800

    new file:   .vscode/extensions.json 	new file:   data/cora/cora/processed/data.pt 	new file:   data/cora/cora/processed/pre_filter.pt 	new file:   data/cora/cora/processed/pre_transform.pt 	new file:   data/cora/cora/raw/ind.cora.allx 	new file:   data/cora/cora/raw/ind.cora.ally 	new file:   data/cora/cora/raw/ind.cora.graph 	new file:   data/cora/cora/raw/ind.cora.test.index 	new file:   data/cora/cora/raw/ind.cora.tx 	new file:   data/cora/cora/raw/ind.cora.ty 	new file:   data/cora/cora/raw/ind.cora.x 	new file:   data/cora/cora/raw/ind.cora.y 	new file:   data/data_construct.ipynb 	new file:   data/for_meta/cora/cora_individual_infection_sir.npy 	new file:   data/for_meta/wiki/Wiki-2.txt 	modified:   main.py 	modified:   network dynamics/SIR_torch.ipynb

--- main.py
+++ main.py
@@ -1,2 +1,7 @@
+import torch
+from torch_geometric.utils import dense_to_sparse
 if __name__ == "__main__":
+  
+    device = torch.device('cuda:0' if torch.cuda.is_available() else 'cpu')
+    print(device)
     print('hello world')
